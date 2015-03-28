93c25beb04f6c9799914546f6dc0ff3e40c7f951
okanp <b3g1nn3r@gmail.com>
2015-03-28 15:32:29 +0200

    First commit

---
+++ Assets/TrioScripts/destroyobject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyobject : MonoBehaviour {
+
+	public float yokolmasaniye;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy (gameObject, yokolmasaniye);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	
+	}
+}
---
+++ Assets/TrioScripts/mouseDrag.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseDrag : MonoBehaviour {
+
+	float distance = 20;
+
+	void OnMouseDrag()
+	{
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y+40, 3);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+		transform.position = objPosition;
+
+
+	}
+}
