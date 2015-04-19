ffa5edb10c5013fde690f5ef2643feaa115fa960
okanp <b3g1nn3r@gmail.com>
2015-04-19 13:35:32 +0300

    objelerin yukselisi
    biraz daha smooth oldu ama hala istedigimiz gibi degil.
    Math.Lerp

--- Assets/TrioScripts/ObjectBehavior.cs
+++ Assets/TrioScripts/ObjectBehavior.cs
@@ -6,30 +6,26 @@ public class ObjectBehavior : MonoBehaviour {
 	//private int collisionCount = 0;
 
 	public static float defaultZ = -17.0f;
-
-	private Gamer gamer = new Gamer();
-
+	
 	private float initHeight;
-
-	private bool dragged;
-
+	
 	// Use this for initialization
 	void Start () {
 		initHeight = transform.position.y;
-		dragged = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print ("Camera yukseldi : " + MainCameraScript.CameraInc);
+		float currentObjectHeight = initHeight + MainCameraScript.CameraInc;
+		float newHeight = Mathf.Lerp(transform.position.y, currentObjectHeight, Time.deltaTime * 100);
 		if (transform.position.z != defaultZ)
-			transform.position = new Vector3(transform.position.x, initHeight + MainCameraScript.CameraInc, transform.position.z);
+			transform.position = transform.position = new Vector3(transform.position.x, newHeight, transform.position.z);;
 	}
 
 	void OnCollisionEnter(Collision col)
 	{
-		gameObject.name = "Destroyed" + gameObject.name;
 		print ("Destroying " + gameObject.name);
+		gameObject.name = "Destroyed" + gameObject.name;
 		DestroyObject (gameObject);	
 	}
 	
