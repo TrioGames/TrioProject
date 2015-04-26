7b94534fb00abe57acac448b92a506f570fc4f33
okanp <b3g1nn3r@gmail.com>
2015-04-26 17:08:44 +0300

    toptan commit
    asd

--- Assets/TrioScripts/BallBehaviour.cs
+++ Assets/TrioScripts/BallBehaviour.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+//using UnityEngine.UI;
 
 public class BallBehaviour : MonoBehaviour {
-
+	public AudioSource ses1;
+	public GUIText CountText;
 	public static float max_height = 0;
+	private int count = 0;
 	// Use this for initialization
 	void Start () {
 	}
@@ -20,6 +23,10 @@ public class BallBehaviour : MonoBehaviour {
 		//if (col.gameObject.tag == "TrioObject" || col.gameObject.tag == "Platform") {
 		if (col.gameObject.tag == "TrioObject") {
 			rigidbody.velocity = new Vector3(rigidbody.velocity.x, 5, rigidbody.velocity.z); 
+			ses1.Play();
+			count++;
+			CountText.text = "Puan: " + count.ToString();
+
 		}
 
 		if (col.gameObject.name == "RespawnPlane")
